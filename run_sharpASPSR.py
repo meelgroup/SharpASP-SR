2f3431c89e2ecca7b08dc4e9abe6b7db70830b50
mahi045 <mahibuet045@gmail.com>
2026-04-19 01:46:13 +1000

    some change in the code.

--- run_sharpASPSR.py
+++ run_sharpASPSR.py
@@ -1,116 +1,152 @@
-import os, argparse, subprocess, shutil
+#!/usr/bin/env python3
+
+import argparse
+import shutil
+import subprocess
+import sys
+import time
+from pathlib import Path
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i','--i', help='input ASP program', required=True)
-parser.add_argument('-t','--t', help='timeout', default=5000, required=False)
+parser.add_argument("-i", "--input", required=True, help="input ASP program")
+parser.add_argument("-t", "--timeout", type=float, default=5000, help="timeout in seconds")
 args = parser.parse_args()
-ganak_binary = 'ganak'
-clark = 'clark'
-if not os.path.exists('./{0}'.format(ganak_binary)):
-    print("Tool {0} does not exist".format(ganak_binary))
-    exit(1)
 
-if shutil.which("gringo"):
-    # print("Gringo Installed")
-    pass
-else:
+ganak_binary = Path("./ganak")
+clark_binary = Path("./clark")
+input_file = Path(args.input)
+
+if not ganak_binary.exists():
+    print(f"Tool {ganak_binary} does not exist")
+    sys.exit(1)
+
+if shutil.which("gringo") is None:
     print("gringo is not installed. Please install gringo")
-    exit(1)
+    sys.exit(1)
 
-if not os.path.exists('./{0}'.format(clark)):
-    print("Tool {0} does not exist".format(clark))
-    exit(1)
+if not clark_binary.exists():
+    print(f"Tool {clark_binary} does not exist")
+    sys.exit(1)
 
-if not os.path.exists(args.i):
-    print('input ' + args.i + ' does not exist')
-    exit(1)
-
-os.system('gringo -o smodels {0} | ./clark -dimacs -output {0}'.format(args.i))
-
-if not os.path.exists('model_' + args.i + '.out'):
-    print('Cannot compute formulas phi_1 and phi_2 !!!')
-    print('Some issue with input file')
-    print('model_' + args.i + '.out does not exist')
-    exit(1)
-
-if not os.path.exists('non_sm_' + args.i + '.out'):
-    print('Cannot compute formulas phi_1 and phi_2 !!!')
-    print('Some issue with input file')
-    print('non_sm_' + args.i + '.out does not exist')
-    exit(1)
-
-def get_time(file, solver):
-    file_name = os.path.basename(file)
-    res_dir = "."
-    output_file = res_dir + "/" + solver + "_" + \
-        file_name[len("output_"):-len(".out")] + ".timeout"
-    time = subprocess.Popen('grep "User time (seconds)" {0}'.format(
-        output_file), shell=True, stdout=subprocess.PIPE).stdout
-    time = time.read().decode("utf-8").strip().split()
-    user_time = float(time[-1])
-
-    time = subprocess.Popen('grep "System time (seconds)" {0}'.format(
-        output_file), shell=True, stdout=subprocess.PIPE).stdout
-    time = time.read().decode("utf-8").strip().split()
-    system_time = float(time[-1])
-    return user_time + system_time
+if not input_file.exists():
+    print(f"input {input_file} does not exist")
+    sys.exit(1)
 
 
-total_time = int(args.t)
-first_count = None
-first_time = None
-second_count = None
-second_time = None
-
-print('The formulas phi_1 and phi_2 are computed already !!!')
-# count the models of the first formula
-# print('timeout {0}s /usr/bin/time --verbose -o first_{2}.timeout ./{1} -v 0 --maxcache 4000 model_{2}.out > output_{2}.out'.format(total_time, ganak_binary, args.i))
-print('Invoking the first counter call ... ')
-os.system('timeout {0}s /usr/bin/time --verbose -o first_{2}.timeout ./{1} --verb 0 --prob 0 --maxcache 4000 model_{2}.out > output_{2}.out'.format(total_time, ganak_binary, args.i))
-
-out_file = open('output_{0}.out'.format(args.i))
-for line in out_file:
-    if line.startswith("c s exact arb int"):
-        l = line.strip().split()
-        first_count = int(l[-1])
-
-out_file.close()
-if first_count == None:
-    # indicate unsolved
-    print("SharpASP-SR timeouted")
-    exit(1)
-else:
-    first_time = get_time("output_{0}.out".format(args.i), "first")
-    print("SharpASP-SR: first count done, count: {0} and time: {1}".format(first_count, first_time))
+def run_command(cmd, stdout_file=None):
+    try:
+        if stdout_file is None:
+            result = subprocess.run(cmd, check=True, text=True, capture_output=True)
+            return result.stdout
+        else:
+            with open(stdout_file, "w", encoding="utf-8") as f:
+                subprocess.run(cmd, check=True, text=True, stdout=f)
+            return None
+    except subprocess.CalledProcessError as e:
+        print("Command failed:")
+        print(" ".join(str(x) for x in cmd))
+        if e.stderr:
+            print(e.stderr)
+        sys.exit(1)
 
 
-# count the (projected) models of the second formula
-total_time = total_time - first_time # remaining time
-if (total_time <= 0):
-    # indicate unsolved
-    print("SharpASP-SR timeouted")
-    exit(1)
+def parse_count(output_file):
+    with open(output_file, "r", encoding="utf-8") as f:
+        for line in f:
+            if line.startswith("c s exact arb int"):
+                parts = line.strip().split()
+                return int(parts[-1])
+    return None
 
-print('Invoking the second counter call ... ')
-# print('timeout {0}s /usr/bin/time --verbose -o second_{2}.timeout ./{1} -v 0 --maxcache 4000 non_sm_{2}.out > output_{2}.out'.format(total_time, ganak_binary, args.i))
-os.system('timeout {0}s /usr/bin/time --verbose -o second_{2}.timeout ./{1} --verb 0 --prob 0 --maxcache 4000 non_sm_{2}.out > output_{2}.out'.format(total_time, ganak_binary, args.i))
-out_file = open('output_{0}.out'.format(args.i))
-for line in out_file:
-    if line.startswith("c s exact arb int"):
-        l = line.strip().split()
-        second_count = int(l[-1])
 
-out_file.close()
-if second_count == None:
-    # indicate unsolved
-    print("SharpASP-SR timeouted")
-    exit(1)
-else:
-    second_time = get_time("output_{0}.out".format(args.i), "second")
-    print("SharpASP-SR: second count done, count: {0} and time: {1}".format(second_count, second_time))
+def run_ganak(formula_file, output_file, timeout_seconds):
+    start = time.time()
+    try:
+        with open(output_file, "w", encoding="utf-8") as f:
+            subprocess.run(
+                [
+                    str(ganak_binary),
+                    "--verb", "0",
+                    "--prob", "0",
+                    "--maxcache", "4000",
+                    str(formula_file),
+                ],
+                check=True,
+                text=True,
+                stdout=f,
+                timeout=timeout_seconds,
+            )
+    except subprocess.TimeoutExpired:
+        print("SharpASP-SR timed out")
+        sys.exit(1)
+    except subprocess.CalledProcessError:
+        print("SharpASP-SR failed while running ganak")
+        sys.exit(1)
 
-if first_count != None and second_count != None:
-    print("SharpASP-SR: Number of answer sets: {0}".format(first_count - second_count))
-    print("SharpASP-SR: Total time: {0}".format(first_time + second_time))
+    elapsed = time.time() - start
+    count = parse_count(output_file)
+    if count is None:
+        print("SharpASP-SR timed out")
+        sys.exit(1)
 
-os.system('rm second_{0}.timeout first_{0}.timeout'.format(args.i))
+    return count, elapsed
+
+
+# compute phi_1 and phi_2
+with open(input_file, "r", encoding="utf-8") as f:
+    gringo = subprocess.Popen(
+        ["gringo", "-o", "smodels", str(input_file)],
+        stdout=subprocess.PIPE,
+        text=False,
+    )
+    subprocess.run(
+        [str(clark_binary), "-dimacs", "-output", str(input_file)],
+        stdin=gringo.stdout,
+        check=True,
+    )
+    gringo.stdout.close()
+    gringo.wait()
+
+model_file = Path(f"model_{input_file}.out")
+non_sm_file = Path(f"non_sm_{input_file}.out")
+
+if not model_file.exists():
+    print("Cannot compute formulas phi_1 and phi_2 !!!")
+    print("Some issue with input file")
+    print(f"{model_file} does not exist")
+    sys.exit(1)
+
+if not non_sm_file.exists():
+    print("Cannot compute formulas phi_1 and phi_2 !!!")
+    print("Some issue with input file")
+    print(f"{non_sm_file} does not exist")
+    sys.exit(1)
+
+print("The formulas phi_1 and phi_2 are computed already !!!")
+
+total_time = args.timeout
+
+print("Invoking the first counter call ... ")
+first_count, first_time = run_ganak(
+    model_file,
+    f"output_{input_file}.out",
+    total_time,
+)
+print(f"SharpASP-SR: first count done, count: {first_count} and time: {first_time}")
+
+remaining_time = total_time - first_time
+if remaining_time <= 0:
+    print("SharpASP-SR timed out")
+    sys.exit(1)
+
+print("Invoking the second counter call ... ")
+second_count, second_time = run_ganak(
+    non_sm_file,
+    f"output_{input_file}.out",
+    remaining_time,
+)
+print(f"SharpASP-SR: second count done, count: {second_count} and time: {second_time}")
+
+print(f"SharpASP-SR: Number of answer sets: {first_count - second_count}")
+print(f"SharpASP-SR: Total time: {first_time + second_time}")
